4cfb1640656ad34457eeb5990a04731f851cacea
Dozz1e <darkozzie@gmail.com>
2025-09-11 11:44:32 -0300

    Creacion footer y header para curso, con boton atras en header y boton comprar en footer. Arreglos menores padding

--- src/lib/menu.ts
+++ src/lib/menu.ts
@@ -17,4 +17,23 @@ const menu = [
   },
 ];
 
-export { menu };
+const menuMobile = [
+  {
+    name: "Inicio",
+    href: "/",
+  },
+  {
+    name: "Cursos",
+    href: "/categorias/masoterapia",
+  },
+  {
+    name: "Diplomado",
+    href: "/cursos/diplomado-masoterapia/",
+  },
+  {
+    name: "Whatsapp",
+    href: "https://wa.me/56944871048?text=Hola%20me%20gustaría%20contactarme",
+  },
+];
+
+export { menu, menuMobile };
